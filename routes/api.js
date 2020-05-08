7e725748e4118975bb564aac26123ef7608f40e0
Glitch (rose-ripple-trust) <none>
2020-05-08 17:11:59 +0000

    👌🖐 Checkpoint ./routes/api.js:15407664/28

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ module.exports = function (app) {
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
-      res.json({
+      res.send({
         'hello':'hello'
       })
     });
